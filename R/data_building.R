5ff2c2f23337a3ef82a233c5ca8669cc7091b09a
Tim Bender <bendertj@gmail.com>
2024-03-30 09:38:41 -0400

    initial commit

---
+++ R/data_building.R
@@ -0,0 +1,12 @@
+library(renv)
+library(readxl)
+#library(remotes)
+library(censusxy)
+#remotes::install_github("chris-prener/censusxy")
+
+renv::snapshot()
+renv::status()
+
+# 
+rm(list=ls());cat('\f')
+gc()
