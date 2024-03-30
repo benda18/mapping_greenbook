0a595da48eb02bef8aab655b08f202fe288129c6
Tim Bender <bendertj@gmail.com>
2024-03-30 10:31:31 -0400

    renv; data

--- R/data_building.R
+++ R/data_building.R
@@ -1,5 +1,6 @@
 library(renv)
 library(readxl)
+library(xlsx)
 #library(remotes)
 library(censusxy)
 #remotes::install_github("chris-prener/censusxy")
@@ -20,6 +21,23 @@ gb$oneline <- paste(gb$Address,
                     gb$City, 
                     gb$State, sep = ", ")
 
-temp.addr <- censusxy::cxy_oneline("7318 Overland Park Court, West Chester, OH")
+for(i in 1:nrow(gb)){
+  if(is.na(gb$cen_addr[i])){
+    temp <- censusxy::cxy_oneline(gb[i,]$oneline)
+    if(is.null(temp)){
+      gb$cen_addr[i] <- "[none found]"
+    }else{
+      gb$cen_addr[i]   <- temp$matchedAddress
+      gb$cen_city[i]   <- temp$addressComponents.city
+      gb$cen_lon[i]    <- temp$coordinates.x
+      gb$cen_lat[i]    <- temp$coordinates.y
+      gb$cen_state[i]  <- temp$addressComponents.state
+      gb$cen_street[i] <- temp$addressComponents.streetName
+      gb$cen_zip[i]    <- temp$addressComponents.zip
+      
+    }
+  }
+  rm(temp)
+}
 
-temp.addr
+
