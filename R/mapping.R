feee8106f8f0e914674596b18e41b2016db742b4
Tim Bender <bendertj@gmail.com>
2024-03-30 11:09:18 -0400

    mapping

--- R/mapping.R
+++ R/mapping.R
@@ -23,6 +23,13 @@ gb_citylabs <- gb |>
             lat = mean(cen_lat, na.rm = T), 
             n = n()) 
 
+# get cities
+sf_cities.al <- tigris::county_subdivisions(state = "AL", cb = T)
+sf_cities.al$green <- F
+sf_cities.al[sf_cities.al$NAME %in% gb$City & 
+                            sf_cities.al$STATE_NAME == "Alabama",]$green <- T
+
+
 # get states
 
 sf_states <- tigris::states(cb = T) 
@@ -30,9 +37,10 @@ sf_states <- sf_states[sf_states$NAME %in% gb$State,]
 
 ggplot() + 
   geom_sf(data = sf_states) +
+  geom_sf(data = sf_cities.al, aes(fill = green))+
   geom_point(data = gb, 
              aes(x = cen_lon, y = cen_lat)) +
-  geom_text_repel(data = gb_citylabs, 
-                  aes(x = lon, y = lat, 
-                      label = City)) +
+  # geom_text_repel(data = gb_citylabs, 
+  #                 aes(x = lon, y = lat, 
+  #                     label = City)) +
   theme_void()
