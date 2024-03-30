9b437e6389587970ba322bd75d7fad6c0ed56b75
Tim Bender <bendertj@gmail.com>
2024-03-30 10:11:28 -0400

    address_oneline logic

--- R/data_building.R
+++ R/data_building.R
@@ -3,6 +3,8 @@ library(readxl)
 #library(remotes)
 library(censusxy)
 #remotes::install_github("chris-prener/censusxy")
+library(janitor)
+
 
 renv::snapshot()
 renv::status()
@@ -10,4 +12,11 @@ renv::status()
 rm(list=ls());cat('\f')
 gc()
 
-# 
+# read spreadsheet----
+
+gb <- read_xlsx(path = "data/greenbook_addresses.xlsx")
+
+gb$oneline <- paste(gb$Address, 
+                    gb$City, 
+                    gb$State, sep = ", ")
+
