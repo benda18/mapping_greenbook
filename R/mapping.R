d147854da38909e908b35dd972da13a34ab94fc7
Tim Bender <bendertj@gmail.com>
2024-03-30 10:48:19 -0400

    renv, mapping

--- R/mapping.R
+++ R/mapping.R
@@ -4,7 +4,23 @@ library(tigris)
 library(xlsx)
 library(readxl)
 library(janitor)
+library(ggrepel)
 
 
 renv::status()
 renv::snapshot()
+
+rm(list=ls());cat('\f')
+gc()
+
+gb <- read_xlsx("data/greenbook_addresses.xlsx")
+
+# get states
+
+sf_states <- tigris::states(cb = T) 
+sf_states <- sf_states[sf_states$NAME %in% gb$State,]
+
+ggplot() + 
+  geom_sf(data = sf_states) +
+  geom_point(data = gb, 
+             aes(x = cen_lon, y = cen_lat))
