f31701f6af4cc3b35b5a4c95c790389fd7154e1f
Tim Bender <bendertj@gmail.com>
2024-03-31 12:18:57 -0400

    data

--- R/mapping.R
+++ R/mapping.R
@@ -16,6 +16,20 @@ gc()
 
 gb <- read_xlsx("data/greenbook_citysummary.xlsx")
 
+# tidy
+gb$lodging[is.na(gb$lodging)] <- 0
+gb$dining[is.na(gb$dining)] <- 0
+gb$beauty[is.na(gb$beauty)] <- 0
+gb$entertainment[is.na(gb$entertainment)] <- 0
+gb$automotive[is.na(gb$automotive)] <- 0
+
+gb$total[is.na(gb$total)] <- gb$lodging[is.na(gb$total)] + 
+  gb$dining[is.na(gb$total)] + 
+  gb$beauty[is.na(gb$total)] + 
+  gb$beauty[is.na(gb$total)] + 
+  gb$entertainment[is.na(gb$total)] + 
+  gb$automotive[is.na(gb$total)]
+
 # get cities
 
 tigris.cities <- NULL
