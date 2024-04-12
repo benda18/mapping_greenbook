ca4fa306f47583e61b589d6986b97c6764d3741c
Tim Bender <bendertj@gmail.com>
2024-04-12 16:16:57 -0400

    ocr commit

--- R/ocr_gb.R
+++ R/ocr_gb.R
@@ -10,10 +10,31 @@ library(janitor)
 
 #renv::snapshot()
 
+#https://cran.r-project.org/web/packages/tesseract/index.html
+# https://digitalcollections.nypl.org/items/f62ceaf0-847a-0132-6c49-58d385a7bbd0
+
+"https://iiif-prod.nypl.org/index.php?id=5206459&t=v"
+
 rm(list=ls());cat('\f')
 gc()
 
 getwd()
 
-tesseract_info()
-tesseract_download("eng", datapath = "data/dict/")
+
+#tesseract_download("eng", datapath = "data/dict/")
+
+# example----
+eng <- tesseract("eng")
+text <- tesseract::ocr("http://jeroen.github.io/images/testocr.png", engine = eng)
+cat(text)
+results <- tesseract::ocr_data("http://jeroen.github.io/images/testocr.png", engine = eng)
+results
+
+# greenbook----
+
+gba <- read_xlsx("data/greenbook_addresses.xlsx")
+gbs <- read_xlsx("data/greenbook_citysummary.xlsx")
+
+eng <- tesseract("eng")
+ct1 <- ocr("img/ct3.png", engine = eng)
+cat(ct1)
