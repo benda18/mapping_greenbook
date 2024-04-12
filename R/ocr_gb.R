13744d14c4c67413ec9726dc329ff1b0aa94f4cf
Tim Bender <bendertj@gmail.com>
2024-04-12 15:54:24 -0400

    data dictionary download

--- R/ocr_gb.R
+++ R/ocr_gb.R
@@ -1,2 +1,19 @@
 library(renv)
 library(tesseract)
+library(dplyr)
+library(readxl)
+library(xlsx)
+#library(remotes)
+#library(censusxy)
+#remotes::install_github("chris-prener/censusxy")
+library(janitor)
+
+#renv::snapshot()
+
+rm(list=ls());cat('\f')
+gc()
+
+getwd()
+
+tesseract_info()
+tesseract_download("eng", datapath = "data/dict/")
