58752fee8a3e63f212bc58aed5a0d40f9ff0454f
Tim Bender <bendertj@gmail.com>
2024-04-17 09:27:58 -0400

    added oklahoma data

---
+++ R/transpose_okla.R
@@ -0,0 +1,26 @@
+# transpose oklahoma data
+
+library(renv)
+library(readxl)
+library(xlsx)
+#library(remotes)
+#library(censusxy)
+#remotes::install_github("chris-prener/censusxy")
+library(janitor)
+
+
+renv::snapshot()
+renv::status()
+
+rm(list=ls());cat('\f')
+gc()
+
+
+# gbsum <- read_xlsx("data/greenbook_citysummary.xlsx")
+
+
+# OLDER----
+# read spreadsheet
+
+okla <- readxl::read_xlsx(path = "data/greenbook_addresses.xlsx", 
+                          sheet = "others")
