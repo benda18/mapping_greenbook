4daeea05e7dc61a8f58049166f0fa08d739e9c73
Tim Bender <bendertj@gmail.com>
2024-04-19 13:28:36 -0400

    sundowns

---
+++ R/sundown towns.R
@@ -0,0 +1,10 @@
+# sundown towns
+
+library(dplyr)
+library(tigris)
+library(ggplot2)
+library(leaflet)
+
+renv::snapshot()
+
+# https://justice.tougaloo.edu/sundown-towns/using-the-sundown-towns-database/state-map/
