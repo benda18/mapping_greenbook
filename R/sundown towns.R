cc794f0a799f115de82cd757ea1849f7c9f44f1f
Tim Bender <bendertj@gmail.com>
2024-04-19 13:52:29 -0400

    tidycensus

--- R/sundown towns.R
+++ R/sundown towns.R
@@ -4,7 +4,17 @@ library(dplyr)
 library(tigris)
 library(ggplot2)
 library(leaflet)
+library(renv)
+library(tidycensus)
+
 
 renv::snapshot()
 
 # https://justice.tougaloo.edu/sundown-towns/using-the-sundown-towns-database/state-map/
+
+rm(list=ls());cat('\f')
+
+
+counties <- tigris::counties(state = state.abb[!state.abb %in% c("AK", "HI")], 
+                             cb = T)
+
