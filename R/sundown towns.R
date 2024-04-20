62e1f004cdf8921d5edd34997a9e011f8fac62e9
Tim Bender <bendertj@gmail.com>
2024-04-19 20:36:26 -0400

    data

--- R/sundown towns.R
+++ R/sundown towns.R
@@ -6,15 +6,87 @@ library(ggplot2)
 library(leaflet)
 library(renv)
 library(tidycensus)
+library(leaflet)
+library(sf)
 
-
-renv::snapshot()
+#renv::snapshot()
 
 # https://justice.tougaloo.edu/sundown-towns/using-the-sundown-towns-database/state-map/
 
 rm(list=ls());cat('\f')
 
 
-counties <- tigris::counties(state = state.abb[!state.abb %in% c("AK", "HI")], 
-                             cb = T)
+expelled.places <- tigris::places( cb = T) 
+expelled.counties <- tigris::counties(cb = T) 
 
+expelled.counties <- expelled.counties[paste(expelled.counties$NAME, 
+                        expelled.counties$STUSPS, sep = ", ") %in% 
+                    c("Comanche, TX", "Greene, IN", 
+                      "Marshall, KY", "Forsyth, GA"),] 
+
+expelled.places <- expelled.places[paste(expelled.places$NAME, 
+                      expelled.places$STUSPS, sep = ", ") %in% 
+                  c("Portsmouth, OH", 
+                    "Wyandotte, MI", "Pollock, LA", 
+                    "Colfax, LA",
+                    "Celina, TN", 
+                    "Paragould, AR", 
+                    "Lexington, OK", 
+                    "Blackwell, OK", 
+                    "Monett, MO", 
+                    "Linton, IN", 
+                    "Elwood, IN", 
+                    "Wilmington, NC", 
+                    "Pana, IL", 
+                    "Carterville, IL", 
+                    "Mena, AR", 
+                    "Pierce City, MO", 
+                    "Decateur, IN", 
+                    "Joplin, MS", 
+                    "Sour Lake, TX", 
+                    "Harrison, AR", 
+                    "Cotter, AR" , 
+                    "Anna, IL", 
+                    "Jonesboro, IL", 
+                    "East St. Louis, IL", 
+                    "Corbin, KY", 
+                    "Ocoee, FL",
+                    "Tulsa, OK", 
+                    "Jay, FL", 
+                    "Rosewood, FL", 
+                    "Blanford, IN", 
+                    "Manhattan Beach, CA", 
+                    "Vienna, IL", 
+                    "Sheridan, AR"),]
+
+ggplot() + 
+  geom_sf(data = expelled.counties) +
+  geom_sf(data = expelled.places) 
+
+df.polygons <- NULL
+
+#View(expelled.counties$geometry)
+
+out.df <- NULL
+for(i in 1:length(expelled.counties$geometry)){
+  temp <- expelled.counties$geometry[[i]][[1]][[1]] %>% 
+    as.data.frame()
+  colnames(temp) <- c("lon", "lat")
+  temp$county <- expelled.counties$NAME[i]
+  temp$state  <- expelled.counties$STUSPS[i]
+  
+  out.df <- rbind(out.df, 
+                  temp)
+  
+}
+out.df
+library(data.table)
+
+leaflet::leaflet() %>%
+  addProviderTiles(
+    "OpenStreetMap",
+    # give the layer a name
+    group = "OpenStreetMap"
+  ) %>%
+  addPolygons(data = expelled.counties) %>%
+  addPolygons(data = expelled.places)
