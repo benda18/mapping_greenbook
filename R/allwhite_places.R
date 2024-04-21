8dd3af029bac7bdaf6ba3d72ff85ad7d42e40026
Tim Bender <bendertj@gmail.com>
2024-04-21 19:10:11 -0400

    commit

--- R/allwhite_places.R
+++ R/allwhite_places.R
@@ -9,13 +9,12 @@ library(tidycensus)
 library(leaflet)
 library(sf)
 library(readr)
+library(data.table)
 
 rm(list=ls());cat('\f')
 gc()
 
-pl.est <- tidycensus::get_estimates(geography = "place")
-
-args(get_estimates)
+#pl.est <- tidycensus::get_estimates(geography = "place", geometry = T)
 
 acs5.vars <- tidycensus::load_variables(dataset = "acs5", year = 2022)
 
@@ -31,7 +30,44 @@ cw.racevars <- data.frame(variable = theones,
                           race = c("total", "white"))
 
 data.race.pl <- tidycensus::get_acs(geography = "place", 
-                    variables = theones)
+                                    variables = theones, 
+                                    geometry = T)
 
 data.race.co <- tidycensus::get_acs(geography = "county", 
-                                    variables = theones)
+                                    variables = theones, 
+                                    geometry = T)
+
+
+race.pl <- left_join(data.race.pl, 
+                     cw.racevars) %>%
+  as.data.table() %>%
+  dcast(., 
+         geometry+NAME ~ race, value.var = "estimate") %>%
+  as.data.frame() %>%
+  as_tibble() %>%
+  mutate(., 
+         pct_white = white/total, 
+         n_notwhite = total-white)
+
+race.co <- left_join(data.race.co, 
+                     cw.racevars) %>%
+  as.data.table() %>%
+  dcast(., 
+        NAME ~ race, value.var = "estimate") %>%
+  as.data.frame() %>%
+  as_tibble() %>%
+  mutate(., 
+         pct_white = white/total, 
+         n_notwhite = total-white)
+
+
+# top N percentage white OR less than 100 black ppl
+top_n      <- 0.01
+pct.cutoff <- 0.95
+n.cutoff   <- 10
+
+counties.out <- race.co[race.co$pct_white >= pct.cutoff |
+                          race.co$n_notwhite <= n.cutoff,]
+
+places.out <- race.pl[race.pl$pct_white >= pct.cutoff |
+                        race.pl$n_notwhite <= n.cutoff,]
